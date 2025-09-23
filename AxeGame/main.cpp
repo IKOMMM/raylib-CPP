93a6ebc91bbe997107efcc007ca818f767fb0460
Mikolaj <mikolaj@koffeecup.net>
2025-09-23 14:42:19 +0200

    C12.AxeGame Data Types and Varables 2

--- AxeGame/main.cpp
+++ AxeGame/main.cpp
@@ -1,6 +1,11 @@
 #include "raylib.h"
 
 int main(){
+
+    float root_beer = 1.99;
+    double cheese_burger = 5.99;
+    bool shouldHaveLunch = true;
+
     int width = 350;
     int height = 200;
     InitWindow(width, height, "Axe Game Miko");
