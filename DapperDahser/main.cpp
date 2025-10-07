6aa7ede5fc2cd602b68e122e48b2c5ad42e2a5a3
Mikolaj <mikolaj@koffeecup.net>
2025-10-07 14:50:16 +0200

    C28. Velocity and Jumping

--- DapperDahser/main.cpp
+++ DapperDahser/main.cpp
@@ -4,7 +4,13 @@ int main(){
     //window dimensions 
     const int windowWidth{512};
     const int windowHeight{380};
-    const int targetFPS{60};
+    const int targetFPS{60};    
+
+    //rectangle dimensions
+    const int width{50};
+    const int height{80};
+    int posY{windowHeight-height};
+    int velocity{-10};
 
     InitWindow(windowWidth, windowHeight, "DD Game");
     SetTargetFPS(targetFPS);
@@ -13,6 +19,12 @@ int main(){
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
+
+        if(IsKeyPressed(KEY_SPACE)){
+            velocity -= 10;
+        }
+
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
 
         // stop drawing
         EndDrawing();
